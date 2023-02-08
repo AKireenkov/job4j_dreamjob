969a4594f21c71a050c28b5ab4bbc7cf4c068ab8
Киреенков Андрей <AKireenkov@alfabank.ru>
2023-02-08 16:27:11 +0300

    4. Многопоточность в базе данных [#504860]

---
+++ src/test/java/ru/job4j/dreamjob/repository/Sql2oUserRepositoryTest.java
@@ -0,0 +1,91 @@
+package ru.job4j.dreamjob.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2oException;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.List;
+import java.util.Properties;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.empty;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class Sql2oUserRepositoryTest {
+    private static Sql2oUserRepository sql2oUserRepository;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        var properties = new Properties();
+        try (var inputStream = Sql2oUserRepositoryTest.class.getClassLoader().getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        var url = properties.getProperty("datasource.url");
+        var username = properties.getProperty("datasource.username");
+        var password = properties.getProperty("datasource.password");
+
+        var configuration = new DatasourceConfiguration();
+        var datasource = configuration.connectionPool(url, username, password);
+        var sql2o = configuration.databaseClient(datasource);
+
+        sql2oUserRepository = new Sql2oUserRepository(sql2o);
+    }
+
+    @AfterEach
+    public void clearUsers() {
+        var users = sql2oUserRepository.findAll();
+        for (var user : users) {
+            sql2oUserRepository.deleteById(user.getId());
+        }
+    }
+
+
+    @Test
+    public void whenSaveThenGetSame() {
+        var user = sql2oUserRepository.save(new User(0, "email@email.ru", "name", "password")).get();
+        var savedUser = sql2oUserRepository.findByEmailAndPassword(user.getEmail(), user.getPassword()).get();
+        assertThat(savedUser).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    public void whenEmailIsDuplicated() {
+        sql2oUserRepository.save(new User(0, "email@email.ru", "name", "password"));
+        var user = new User(0, "email@email.ru", "name1", "password1");
+        assertThatThrownBy(() -> sql2oUserRepository.save(user))
+                .isInstanceOf(Sql2oException.class)
+                .hasMessageContaining("Error in executeUpdate, Нарушение уникального индекса или первичного ключа:");
+    }
+
+    @Test
+    public void whenSaveSeveralThenGetAll() {
+        var user1 = sql2oUserRepository.save(new User(0, "email1@email.ru", "name1", "password")).get();
+        var user2 = sql2oUserRepository.save(new User(0, "email2@email.ru", "name2", "password")).get();
+        var user3 = sql2oUserRepository.save(new User(0, "email3@email.ru", "name3", "password")).get();
+        var result = sql2oUserRepository.findAll();
+        assertThat(result).isEqualTo(List.of(user1, user2, user3));
+    }
+
+    @Test
+    public void whenDontSaveThenNothingFound() {
+        assertThat(sql2oUserRepository.findAll()).isEqualTo(emptyList());
+        assertThat(sql2oUserRepository.findByEmailAndPassword("test@test.ru", "123")).isEqualTo(empty());
+    }
+
+    @Test
+    public void whenDeleteThenGetEmptyOptional() {
+        var user = sql2oUserRepository.save(new User(0, "email1@email.ru", "name1", "password")).get();
+        var isDeleted = sql2oUserRepository.deleteById(user.getId());
+        var savedCandidate = sql2oUserRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        assertThat(isDeleted).isTrue();
+        assertThat(savedCandidate.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void whenDeleteByInvalidIdThenGetFalse() {
+        assertThat(sql2oUserRepository.deleteById(0)).isFalse();
+    }
+}
